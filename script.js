1e17c9dbe01775fb99cbe743fe152780196bd8a4
ischmidls <izak.schmidlkofer@gmail.com>
2021-03-18 20:13:59 -0700

    Commit
    Commit

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-let form = document.forms[0];
+// script.js
+alert("Recieved File!");
+
+let form = document.getElementById('fileupload')  
+form.addEventListener(
+	'change', 
+	function () {
+		var fr = new FileReader(); 
+		fr.onload = function () { 
+			let contents = document.getElementById('contents')
+			contents.textContent = this.result;};
+            fr.readAsText(this.files[0]);
+        }
+    );
